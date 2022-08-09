4c1b60f2c573ebe13dcc880a7efb6bcd37d30980
Roberto VS <rvs.git@gmail.com>
2022-08-09 15:00:08 -0400

    added build optimization, next is build speed

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,27 @@
  * ----------------------------------------
  */
 
-// enable the Node´s full path lookup at path.resolve
+// enable the Node file system full path lookup (for path.resolve)
 const path = require("path");
 
 // refer the webpack module (for tinkering)
 const { web, webpack } = require("webpack");
 
-// import html-webpack-plugin module
+// import module
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// import mini-css-extract-plugin
+// import module
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// import module
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+// import module
+const TerserPlugin = require("terser-webpack-plugin");
 
 // change buildMode to the type of build, manually
 // set mode between "development" | "production" | "none"
-const buildMode = "production";
+const buildMode = "development";
 
 
 /* ----------------------------------------
@@ -35,9 +41,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].js",
+        filename: "[name][contenthash].js",
         assetModuleFilename: "images/[name][ext]",
-        //filename: "[name][contenthash].js",
         clean: true,
     },
     module: {
@@ -66,6 +71,20 @@ module.exports = {
             },
         ],
     },
+    optimization: {
+        minimize: true, 
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             inject: false,
@@ -89,6 +108,7 @@ module.exports = {
             favicon: "src/images/favicon/ye-icon.png"
         }),
     ].concat(buildMode !== "production" ? [] : [new MiniCssExtractPlugin()]),
+    devtool: "source-map",
     devServer: {
         static: {
             directory: path.resolve(__dirname, "dist")
